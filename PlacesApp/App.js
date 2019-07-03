6d7d9d770aa7373735160d69f5b8939b787ae983
Neil Patel <neilp980@yahoo.com>
2019-07-03 14:45:19 -0400

    Added first screens

--- PlacesApp/App.js
+++ PlacesApp/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hello men!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgb(255, 0, 0)',
-    alignItems: 'center',
-    justifyContent: 'center',
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+// import the different screens
+import Loading from './screens/Loading'
+import SignUp from './screens/SignUp'
+import Login from './screens/Login'
+import Main from './screens/Main'
+// create our app's navigation stack
+const App = createSwitchNavigator(
+  {
+    Loading,
+    SignUp,
+    Login,
+    Main
   },
-});
+  {
+    initialRouteName: 'Loading'
+  }
+)
+export default createAppContainer(App)
