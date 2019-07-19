9fd3fb197bb08d3ee2d752d2ac3aaf56a19bb01c
tpatel1781 <tpatel1781@gmail.com>
2019-07-19 14:08:33 -0400

    Groups page displays GroupItems for hermitsuan user

--- PlacesApp/components/Groups.js
+++ PlacesApp/components/Groups.js
@@ -5,20 +5,53 @@ import Constants from '../constants'
 
 import { withFirebase } from './firebase';
 import GroupItem from './GroupItem';
+
 class GroupsBase extends React.Component {
 	state = {
-		user: ''
+		user: '', username: '', groups: []
 	}
+	/**
+	 * Put all of the group names for this user in state.groups so GroupItems 
+	 * can be populated with the names
+	 */
 	componentDidMount() {
 		axios.get(Constants.SERVER_URL + "/getUser", {
-
-		})
+			params: {
+				username: 'hermitsuan'
+			}
+			// Pass the username from Firebase to make the getUser call
+		}).then(response => {
+			for (var groupID of response.data.groupIDs) {
+				axios.get(Constants.SERVER_URL + '/getGroup', {
+					params: { groupID: groupID }
+				}).then(response => {
+					console.log("REsponse data: " + JSON.stringify(response.data));
+					this.setState((prevState) => ({
+						groups: prevState.groups.concat([response.data])
+					}));
+					console.log("Added " + response.data)
+					console.log("Group Names: " + this.state.groups);
+				});
+			}
+			// add a group to the hermitsuan user and then try this again
+			// console.log("Group Names:  " + JSON.stringify(this.state.groups));
+			console.log("User data: " + JSON.stringify(response.data));
+		});
 	}
     render() {
+		var groupItemList = [];
+		this.state.groups.forEach(function (group) {
+			groupItemList.push(
+				<GroupItem name={group.name} key={group.groupID} description={group.users.join(', ')} />
+			);
+		}.bind(this));
         return (
             <View style={styles.container}>
 				<Text style={styles.title}>Groups</Text>
-                <GroupItem name='IV Squad' description='Sample description' />
+				{groupItemList}
+                {/* <GroupItem name='IV Squad' description='Sample description' /> */}
+				{/* <GroupItem name='Group 1' description='Starbucks Berlin Turnpike' /> */}
+				{/* <GroupItem name='Group 2' description='Amelias Boston' /> */}
             </View>
         )
     }
