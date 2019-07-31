16584ae3abce509eb158e1c0520678dc05609b5e
tpatel1781 <tpatel1781@gmail.com>
2019-07-31 15:23:47 -0400

    GroupThread page shows group's visited places

--- PlacesApp/components/GroupThread.js
+++ PlacesApp/components/GroupThread.js
@@ -1,28 +1,69 @@
 import React from 'react'
-import { StyleSheet, Platform, Button, Image, Text, View } from 'react-native'
+import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native'
+import axios from 'axios';
+import Constants from '../constants'
+import { Button } from 'react-native-elements';
+
+import PlaceItem from './PlaceItem';
 
 export default class GroupThread extends React.Component {
+	state = {
+		group: '',
+		sortedListOfPlaces: [],
+	}
     componentDidMount() {
-        this.props.navigation.setParams({ swipeEnabled: false })
+		// Store the group in a local object
+		axios.get(Constants.SERVER_URL + '/getGroup', {
+			params: {
+				groupID: this.props.navigation.getParam('groupID', '0') 
+			}
+		}).then(response => {
+			// Get group's list of places, then sort them by date
+			const listOfPlaces = response.data.visitedPlaces;
+			listOfPlaces.sort(function (place1, place2) { 
+				var date1 = new Date(place1.visitedDate);
+				var date2 = new Date(place2.visitedDate);
+				return date1 - date2;
+			});
+			this.setState(() => ({ 
+				group: response.data,
+				sortedListOfPlaces: listOfPlaces
+			}));
+		});
+		
     }
     render() {
-        const groupID = this.props.navigation.getParam('groupID', '0');
+		const groupID = this.props.navigation.getParam('groupID', '0');
+		var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+		var placeItemList = [];
+		this.state.sortedListOfPlaces.forEach(function (place) {
+			placeItemList.push(
+				<PlaceItem
+					placeName={place.name} 
+					key={place.googleID} 
+					visitedDate={(new Date(place.visitedDate)).toLocaleDateString("en-US", options)}
+				/>
+			);
+		}.bind(this));
+
         return (
             <View style={styles.mainContainer}>
-                <Text>{groupID}</Text>
-                <Button onPress={() =>
-                    this.props.navigation.goBack()
-                } title={"GO BACK"} />
+				<ScrollView>{placeItemList}</ScrollView>
+				<Button
+					title="Search"
+					style={{marginBottom: 25}}
+					onPress={() => {}}
+				/>
             </View>
         )
     }
 }
 const styles = StyleSheet.create({
     mainContainer: {
-        marginTop: 50,
-        position: 'absolute',
+        // marginTop: 30,
+        // position: 'absolute',
         height: '100%'
     }
 
 })
-
---
+++ PlacesApp/components/PlaceItem.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, Platform, Button, Image, Text, View, TouchableHighlight } from 'react-native'
+
+export default class PlaceItem extends React.Component {
+	// onPressGroup = () => {
+	// 	this.props.showThread(this.props.groupID);
+	// }
+	render() {
+		return (
+			<TouchableHighlight onPress={this.onPressGroup}>
+				<View style={styles.mainContainer}>
+					<Text style={styles.groupName} numberOfLines={1}>{this.props.placeName}</Text>
+					<Text style={styles.descriptionText} numberOfLines={2}>{this.props.visitedDate}</Text>
+				</View>
+			</TouchableHighlight>
+		)
+	}
+}
+const styles = StyleSheet.create({
+	mainContainer: {
+		marginTop: 30,
+		marginLeft: 24,
+		marginRight: 20,
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+		flexDirection: 'column',
+	},
+	groupName: {
+		fontWeight: 'bold',
+	},
+	descriptionText: {
+	}
+
+})
+
