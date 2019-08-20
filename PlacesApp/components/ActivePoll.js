90955a1450fd062d19c5b211e07009217dfaeda2
tpatel1781 <tpatel1781@gmail.com>
2019-08-20 14:17:52 -0400

    Implemented distance between current location and each place for PollCards

--- PlacesApp/components/ActivePoll.js
+++ PlacesApp/components/ActivePoll.js
@@ -41,7 +41,22 @@ class ActivePollBase extends React.Component {
 					places: tempPlaces
                 });
             })
-    }
+	}
+	
+	calculateDistance(lat1, lon1, lat2, lon2) {
+		if ((lat1 == lat2) && (lon1 == lon2)) { return 0; }
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) { dist = 1; }
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		return parseFloat(dist.toFixed(1));
+	}
+
     render() {
 		var placeItemList = []
 		this.state.places.forEach(function (place) {
@@ -50,7 +65,8 @@ class ActivePollBase extends React.Component {
 					name={place.name}
 					key={place.id}
 					rating={place.rating + ' (' + place.user_ratings_total + ')'}
-					distance={"FIX THIS"}
+					distance={this.calculateDistance(this.state.position.latitude, this.state.position.longitude,
+										place.geometry.location.lat, place.geometry.location.lng) + ' mi'}
 					openStatus={place.opening_hours.open_now}
 					price={place.price_level}
 				/>
