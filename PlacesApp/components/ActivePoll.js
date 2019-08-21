cf067a3b828751f438e81fec753719ece6d69280
Neil Patel <neilp980@yahoo.com>
2019-08-21 12:24:45 -0400

    added poll

--- PlacesApp/components/ActivePoll.js
+++ PlacesApp/components/ActivePoll.js
@@ -9,40 +9,11 @@ import PollCard from './PollCard';
 
 class ActivePollBase extends React.Component {
     state = {
-        places: [],
-        position: '',
     }
     componentDidMount() {
-        navigator.geolocation.getCurrentPosition((position) => {
-            this.setState({ position: { longitude: position.coords.longitude, latitude: position.coords.latitude } });
-            this.generateNewPlaces();
-        }, (error) => {
-            console.log(JSON.stringify(error))
-        }, {
-                enableHighAccuracy: true,
-                timeout: 20000,
-                maximumAge: 1000
-            });
+        
     }
 
-    generateNewPlaces() {
-        axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json' +
-            '?location=' + this.state.position.latitude + ',' + this.state.position.longitude +
-            '&radius=8000' +
-            '&types=restaurant' +
-            '&key=AIzaSyBzD1vJ3QqK6hX-Y9j9Z_NVqNyycC3Aqd4' +
-            '&language=en' + 
-            '&fields='+'name,rating,user_ratings_total,price_level,geometry,opening_hours').then(response => {
-				var tempPlaces = []
-				for (i=0; i<5; i++) {
-					tempPlaces.push(response.data.results[i]);
-				}
-                this.setState({
-					places: tempPlaces
-                });
-            })
-	}
-	
 	calculateDistance(lat1, lon1, lat2, lon2) {
 		if ((lat1 == lat2) && (lon1 == lon2)) { return 0; }
 		var radlat1 = Math.PI * lat1/180;
@@ -59,13 +30,14 @@ class ActivePollBase extends React.Component {
 
     render() {
 		var placeItemList = []
-		this.state.places.forEach(function (place) {
+		this.props.pollPlaces.forEach(function (pollPlace) {
+            const place = pollPlace.place
 			placeItemList.push(
 				<PollCard
 					name={place.name}
 					key={place.id}
 					rating={place.rating + ' (' + place.user_ratings_total + ')'}
-					distance={this.calculateDistance(this.state.position.latitude, this.state.position.longitude,
+					distance={this.calculateDistance(this.props.position.latitude, this.props.position.longitude,
 										place.geometry.location.lat, place.geometry.location.lng) + ' mi'}
 					openStatus={place.opening_hours.open_now}
 					price={place.price_level}
--- PlacesApp/components/ThreadPoll.js
+++ PlacesApp/components/ThreadPoll.js
@@ -15,6 +15,7 @@ class ThreadPollBase extends React.Component {
             longitude: '',
             latitude: '',
         },
+        pollPlaces: [],
         group: "",
         isPollOpen: false,
     }
@@ -26,7 +27,8 @@ class ThreadPollBase extends React.Component {
         }).then(response => {
             this.setState(() => ({
                 group: response.data,
-                isPollOpen: response.data.isPollOpen
+                isPollOpen: response.data.isPollOpen,
+                pollPlaces: response.data.pollPlaces,
             }));
         });
     }
@@ -37,6 +39,7 @@ class ThreadPollBase extends React.Component {
                 pollState: true
             }
         ).then(response => {
+            this.setPositionAndGeneratePoll();
             this.setState({
                 isPollOpen: true,
             })
@@ -54,11 +57,58 @@ class ThreadPollBase extends React.Component {
                 isPollOpen: false,
             })
         })
+
     }
+
+    setPositionAndGeneratePoll() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.setState({ position: { longitude: position.coords.longitude, latitude: position.coords.latitude } });
+            this.generateNewPlaces();
+        }, (error) => {
+            console.log(JSON.stringify(error))
+        }, {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 1000
+            }
+        );
+    }
+
+    generateNewPlaces() {
+        axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json' +
+            '?location=' + this.state.position.latitude + ',' + this.state.position.longitude +
+            '&radius=8000' +
+            '&types=restaurant' +
+            '&key=AIzaSyBzD1vJ3QqK6hX-Y9j9Z_NVqNyycC3Aqd4' +
+            '&language=en' +
+            '&fields=' + 'name,rating,user_ratings_total,price_level,geometry,opening_hours').then(response => {
+                var tempPlaces = []
+                for (i = 0; i < 5; i++) {
+                    tempPlaces.push(response.data.results[i]);
+                }
+                axios.post(Constants.SERVER_URL + '/addPollPlaces',
+                    {
+                        groupID: this.props.navigation.getParam('groupID', '0'),
+                        places: tempPlaces
+                    }
+                ).then(response => {
+                    
+                })
+            });
+    }
+
     render() {
         return (
             <View>
-                {this.state.isPollOpen ? (<ActivePoll stopPoll={() => this.stopPoll()} />) : (<InactivePoll startPoll={() => this.startPoll()} />)}
+                {this.state.isPollOpen ?
+                    (<ActivePoll
+                        pollPlaces={this.state.pollPlaces}
+                        position={this.state.position}
+                        stopPoll={() => this.stopPoll()}
+                    />) :
+                    (<InactivePoll
+                        startPoll={() => this.startPoll()}
+                    />)}
             </View>
         )
     }
